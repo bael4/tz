6d12e373835f563bd4a4adf1a75c527a1544ef13
bael4 <bayel.ryspekov@gmail.com>
2022-12-23 19:10:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+
+const ONE_SECOND = 1000
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const removeItem = useCallback((id) => {
+    setData((prev) => [...prev].filter((item) => item.id !== id));
+  }, []);
+
+  // const startInterval = useCallback(() => {
+  //   setData(data.map(el => {
+  //     el.seconds -= 1;
+  //     return el
+  //   }));
+  // }, [data]);
+
+  const startTimer = useCallback(
+    (timer) => {
+      setTimeout(() => {
+        removeItem(timer.id);
+      }, ONE_SECOND * timer.seconds);
+    },
+    [removeItem]
+  );
+
+const updateTimer = () => {
+    const updatedData = data.map(el => {
+      el.seconds--;
+      return el;
+    });
+    setData(updatedData);
+  };
+
+  const addItem = useCallback(() => {
+    const time = Math.floor(Math.random() * 20) + 11;
+
+    const newData = {
+      message: "Исчезнет через",
+      id: data.length + 1,
+      seconds: time,
+    };
+
+    setData([...data, newData]);
+    startTimer(newData);
+  }, [data, startTimer]);
+
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => updateTimer(), ONE_SECOND);
+    return () => clearInterval(interval);
+  }, [data.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addItem}>Add</button>
+      {
+        data.map((el,index) => {
+          return (
+            <div key={el.id}>
+              {`${index+1} ${el.message} ${el.seconds}`}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
