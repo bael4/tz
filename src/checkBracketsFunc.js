e22f6bbb8ccff213da56bd9c363bf3c25844d080
Bael Ryspekov <109285000+bael4@users.noreply.github.com>
2022-12-25 00:45:31 +0600

    Update checkBracketsFunc.js

--- src/checkBracketsFunc.js
+++ src/checkBracketsFunc.js
@@ -5,23 +5,46 @@
 // строке ([]] — нет. В первом случае правильно расставлено 8 скобок.
 // Во втором случае указаны 2 скобки правильно, и 2 не правильно.
 
+// const func = (str) => {
+//   let correct = 0;
+//   let incorrect = 0;
+//   const br = str.split("");
+//   console.log(br);
+//   br.map((e) => {
+//     if ((e == "(" && e == ")") || (e == "[" && e == "]")) {
+//       correct++;
+//     }
+//     if ((e == "(" && e !== ")") || (e == "[" && e !== "]")) {
+//       incorrect++;
+//     }
+//   });
+
+//   return `правильно расставлено ${correct} скобок, не правильно расставлено  ${incorrect} скобок`;
+// };
+// // let res = func("([]{})[]");
+// let res2 = func("([]");
+
+// console.log(res2);
+
 const func = (str) => {
-  let correct = 0;
-  let incorrect = 0;
-  const br = str.split("");
-  console.log(br);
-  br.map((e) => {
-    if ((e == "(" && e == ")") || (e == "[" && e == "]")) {
-      correct++;
-    }
-    if ((e == "(" && e !== ")") || (e == "[" && e !== "]")) {
-      incorrect++;
-    }
-  });
+ 
+  const stack = []
+  
+  for (let i = 0; i < str.length; i++) {
+    const br = str[i]
+    if(br==='(' || br==='{' || br==='[' ){
+      stack.push(br)
+    }else{
+      const last = stack.pop()
 
-  return `правильно расставлено ${correct} скобок, не правильно расставлено  ${incorrect} скобок`;
+      if(!last){
+        return false
+      }
+    }
+  }
+  if(stack.length)return false
+  return true
+
 };
-// let res = func("([]{})[]");
-let res2 = func("([]");
-
-console.log(res2);
+let res = func("({}[])[]");
+console.log(res);
